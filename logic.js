d1e3909d32482ae14c918e70d563b19e600326df
Bernardo-R <brosario12@live.com>
2023-04-21 14:35:53 -0400

    fixed problem when adding and Deleteing a new Vehicle

--- logic.js
+++ logic.js
@@ -1,63 +1,39 @@
-// Step 1: In your console, type: npm install -g json-server
+const URL = "https://643d8ccb6c30feced81531da.mockapi.io/cars";
 
-//   Step 2: In your console, type: json-server --watch db.json
-//           Your console should look something like this:
-
-//          Resources
-//           http://localhost:3000/carInventory
-
-const URL2 = "https://643d8ccb6c30feced81531da.mockapi.io/cars";
-
-// const URL = "http://localhost:3000/carInventory";
 let btnEl = document.querySelector(".btn");
 let btnUpdate = document.querySelector("#update");
 
-// $.get(URL2).then((data) => {
-//   //gettig/requesting data and displaying it as a table
-//   for (let i = 0; i < data.length; i++) {
-//     // for each data, a row will be created
-//     $(".tbody").append(
-//       `<tr><td>${data[i].id}</td><td>${data[i].Model}</td><td>${data[i].Manufactor}</td><td>${data[i].Type}</td>
-//       <td><button class="btn btn-outline-danger" id="${data[i].id}" onclick="deleteUser(id)">Delete</button></td></tr>`
-//     );
-//   }
-// });
+fetch(URL) //Dispay API Data to the console
+  .then((response) => response.json())
+  .then((data) => {
+    tableData = data;
+    displayVehicleData(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+function displayVehicleData(data) {
+  $(".tbody").empty();
+  for (let i = 0; i < data.length; i++) {
+    let existingRow = $(`.tbody tr[id='${data[i].id}']`);
+    if (existingRow.length) {
+      existingRow.find(".model").text(data[i].Model);
+      existingRow.find(".manufactor").text(data[i].Manufactor);
+      existingRow.find(".type").text(data[i].Type);
+    } else {
+      $(".tbody").append(
+        `<tr id="${data[i].id}"><td>${data[i].id}</td><td class="model">${data[i].Model}</td><td class="manufactor">${data[i].Manufactor}</td><td class="type">${data[i].Type}</td>
+          <td><button class="btn btn-outline-danger" id="${data[i].id}" onclick="deleteVehicle(id)">Delete</button></td></tr>`
+      );
+    }
+  }
+}
 
-// btnEl.addEventListener("click", () => {
-//   //this btn will post/send information gather from add vehile form
-//   $.post(URL2, {
-//     //method .post to send data to database
-//     Model: $("#Model").val(),
-//     Type: $("#Type").val(),
-//     Manufactor: $("#Maker").val(),
-//   });
-// });
-
-// function deleteUser(id) {
-//   // this function will take id as argument; deleting data thats attached to it in database
-//   $.ajax(`${URL}/${id}`, {
-//     type: "DELETE",
-//   });
-// }
-
-// btnUpdate.addEventListener("click", function updateUser() {
-//   //this button on click will update selected vehicle id in database
-//   let id = $("#vehicleId").val(); //stores info from user and hold it in id variable
-
-//   $.ajax(`${URL2}/${id}`, {
-//     //method for retrieving data from database that corresponds to the currently inserted ID
-//     method: "PUT", //PUT; will update/replace data
-//     data: {
-//       Model: $("#updateModel").val(),
-//       Type: $("#updateType").val(),
-//       Manufactor: $("#updateMaker").val(),
-//     },
-//   });
-// });
-
+let tableData = [];
 btnEl.addEventListener("click", () => {
+  //Adding new Vehicle
   console.log("Pls Work");
-  fetch(URL2, {
+  fetch(URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -69,14 +45,28 @@ btnEl.addEventListener("click", () => {
     }),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      tableData.push(data);
+      displayVehicleData(tableData);
+      $("#addVehicle").trigger("reset");
+    })
     .catch((error) => console.error(error));
 });
 
 btnUpdate.addEventListener("click", function updateVehicle() {
+  //Updating selected Vehicle
   let id = $("#vehicleId").val();
 
-  fetch(`${URL2}/${id}`, {
+  // check if the id exists in the tableData array
+  let vehicleToUpdate = tableData.find((vehicle) => vehicle.id === id);
+  if (!vehicleToUpdate) {
+    alert(`No vehicle found with id ${id}`);
+    $("#UpdateVehicle").trigger("reset");
+    return;
+  }
+
+  fetch(`${URL}/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -88,43 +78,40 @@ btnUpdate.addEventListener("click", function updateVehicle() {
     }),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      tableData = tableData.map((vehicle) => {
+        if (vehicle.id === id) {
+          return data;
+        } else {
+          return vehicle;
+        }
+      });
+      displayVehicleData(tableData);
+      $("#UpdateVehicle").trigger("reset");
+    })
     .catch((error) => console.error(error));
 });
 
 function deleteVehicle(id) {
-  fetch(`${URL2}/${id}`, {
+  // Find the index of the vehicle to delete in the tableData array
+  let index = tableData.findIndex((vehicle) => vehicle.id === id);
+
+  // Send the delete request
+  fetch(`${URL}/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      // Remove the deleted vehicle from the tableData array
+      tableData.splice(index, 1);
+
+      // Update the table with the new data
+      displayVehicleData(tableData);
+      console.log(data);
+    })
     .catch((error) => console.error(error));
 }
-
-fetch(URL2)
-  .then((response) => response.json())
-  .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-      $(".tbody").append(
-        `<tr><td>${data[i].id}</td><td>${data[i].Model}</td><td>${data[i].Manufactor}</td><td>${data[i].Type}</td>
-                  <td><button class="btn btn-outline-danger" id="${data[i].id}" onclick="deleteVehicle(id)">Delete</button></td></tr>`
-      );
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
-
-// fetch(`${URL2}/${id}`, {
-//   method: "PUT",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error(error));
